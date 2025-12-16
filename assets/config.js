197d220a4bc862bc814dd245dd507cc9bab41fd3
hengkp <40425349+hengkp@users.noreply.github.com>
2025-12-17 06:38:31 +0700

    Update config.js

--- assets/config.js
+++ assets/config.js
@@ -2,7 +2,7 @@
 window.APP_CONFIG = {
   LIFF_ID: "2008690429-5QiQHOUA",
   // ใส่ URL ของ Apps Script Web App (exec) หลังจาก Deploy แล้ว
-  GAS_WEBAPP_URL: "https://script.google.com/macros/s/AKfycbzmf3Od_pgGMGSRnn7Vn2Txag_7LB85qWn2JSFS8DOe-EculyYv7w99TMOuF88y3n8ykg/exec",
+  GAS_WEBAPP_URL: "https://script.google.com/macros/s/AKfycbwnjTxxpd1xcAHlBHebrb-0KEBvtgMBa9jRabACpom7Igex8mMmqMXJsIq1TYx1uHt3/exec",
   // ลิงก์ OA (เอาไว้ redirect ตอนอยู่นอก LINE)
   OA_URL: "https://lin.ee/7gdLJZ3",
 };

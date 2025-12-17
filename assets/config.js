1be37e929d93cf2aeceb315f6475e3a96ccedebc
hengkp <40425349+hengkp@users.noreply.github.com>
2025-12-17 21:21:57 +0700

    Update config.js

--- assets/config.js
+++ assets/config.js
@@ -1,7 +1,7 @@
 // web/assets/config.js
 window.APP_CONFIG = {
   LIFF_ID: "2008690429-5QiQHOUA",
-  GAS_WEBAPP_URL: "https://script.google.com/macros/s/AKfycbwSXljabi_byqh648vQBfbuEaEAdbEb-uRNe-rOwtqsNPH7NpWd_Jm3FdZTbGk2QIJxbg/exec",
+  GAS_WEBAPP_URL: "https://script.google.com/macros/s/AKfycbxvXUdNX9f7g-ToCzr_7vJBYGn3nHJ4W48kbAVxcDSPOYhuVxzT_Ssa3RloLzhcwWvuVQ/exec",
   OA_URL: "https://lin.ee/AeZIqWe",
   CERT_GOOGLE_FORM_URL: "https://forms.gle/uu8r1GgDXPEGYwgo9"
 };
